17a59a57607cf4ae93b44aa7846eb216a0d99f47
nikhil41425 <nikhil41425@gmail.com>
2022-04-17 19:01:07 +0530

    fgfg

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project1';
+
+  name="nikhil"
+
+  myColor="color"
+
+  myData:any;
+  myModel:any;
+  templateee:any;
+  logInName = 'user'
+
+  myStatement:boolean=false;
+
+  fruits:any[]=["mango","apple","orange"]
+
+  users: any= [
+    {
+    "userId": 1,
+    "userName": 'User1'
+    },
+    {
+    "userId": 2,
+    "userName": 'User2'
+    },
+    ];
+   
+
+  myclick(){
+
+    // this.myData="hello nikhil"
+
+    alert("helloooo")
+
+  }
+}
